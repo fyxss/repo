7b766b6053327c2ece1d781183ec56f66f86e210
fyxss <mdtarekhasanbhuiyan@gmail.com>
2026-02-10 11:48:36 +0600

    testing

---
+++ c.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+  cout << "----" << '\n';
+}
+
+int32_t main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int T; cin >> T;
+  while (T--) solve();
+  return 0;
+}
